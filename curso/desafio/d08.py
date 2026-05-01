662858606fc2fe5b68966f427c5f0343f68b845e
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-05-01 18:00:15 -0300

    desafio pra coverter metros em km, cm etc...

--- curso/desafio/d08.py
+++ curso/desafio/d08.py
@@ -1,5 +1,9 @@
 metros = int(input('Digite valor em metros para converter: '))
+km = metros / 1000
+hm = metros / 100
+dam = metros / 10
+dm = metros * 10
 cm = metros * 100
 ml = metros * 1000
 
-print('{} metros convertido para centimetro: {} cm \n em milimetros: {} ml'.format(metros, cm, ml))
+print('{} metros convertido para centimetro: {} cm \n em milimetros: {} mm \n decímetro {} dm \n decâmetro {} dam \n hectômetro {} hm \n quilômetro {} km'.format(metros, cm, ml,dm , dam, hm, km ))
