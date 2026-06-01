309cb2469e22f2f3fd2971ad3dcdd4af38a7ebd1
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-06-01 16:20:04 -0300

    manipulando texto, exercicio

---
+++ curso/desafio/salario.py
@@ -0,0 +1,9 @@
+nome = input('Seu nome')
+salarioB = float(input('Salario bruto'))
+salarioL = salarioB - (salarioB * 0.10)
+print(nome, salarioB, salarioL)
+
+nome = input('Seu nome')
+salarioB = float(input('Salario bruto'))
+salarioL = salarioB - (salarioB /10)
+print(nome, salarioB, salarioL)
---
+++ curso/manipulando-texto.py
@@ -0,0 +1,10 @@
+frase = 'Curso em Video Python'
+
+print(frase.replace('Curso', 'Ovo'))
+print(frase.count('o'))
+split = frase.split()
+print(split[2])
+print('-'.join(split))
+print(frase.find('Video'))
+
+print(len(frase))
